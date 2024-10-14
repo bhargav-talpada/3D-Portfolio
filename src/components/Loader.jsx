ec2df514115027dc1037ad37c4ed17bbfb9e71f4
Bhuro11 <bhargavtalpada2004@gmail.com>
2024-10-14 22:42:12 +0530

    loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -6,13 +6,13 @@ const Loader = () => {
 
   return (
     <Html center>
-      <div className="relative w-20 h-20 mt-40 transform-style-3d perspective-800">
+      {/* <div className="relative w-20 h-20 mt-40 transform-style-3d perspective-800">
         <div className="absolute w-full h-full rounded-full border-b-2 border-blue-400 animate-rotate1"></div>
         <div className="absolute w-full h-full rounded-full border-b-2 border-blue-400 animate-rotate2"></div>
         <div className="absolute w-full h-full rounded-full border-b-2 border-blue-400 animate-rotate3"></div>
-      </div>
+      </div> */}
       <span className="canvas-load"></span>
-      {/* <p style={{fontSize: 20, color: '#f1f1f1', fontWeight: 800, marginTop: 40}}>{progress.toFixed(2)}%</p> */}
+      <p style={{fontSize: 20, color: '#f1f1f1', fontWeight: 800, marginTop: 40}}>{progress.toFixed(2)}%</p>
     </Html>
   )
 }
